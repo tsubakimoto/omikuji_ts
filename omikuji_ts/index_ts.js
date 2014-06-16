8add8a54e74c73ebb38eff2a42b4cdb853d5ebe6
tsubakimoto_s <tkmbmbr@gmail.com>
2014-06-16 22:10:16 +0900

    TypeScript版の作成。

---
+++ omikuji_ts/index_ts.js
@@ -0,0 +1,21 @@
+﻿var Omikuji = (function () {
+    function Omikuji(span) {
+        this.span = span;
+        this.items = ["大吉", "中吉", "小吉", "凶", "大凶"];
+    }
+    Omikuji.prototype.consult = function () {
+        var result = Math.floor(Math.random() * this.items.length);
+        this.span.innerHTML = this.items[result];
+    };
+    return Omikuji;
+})();
+
+window.onload = function () {
+    var result = document.getElementById("result");
+    var omikuji = new Omikuji(result);
+    var button = document.getElementById("omikuji");
+    button.onclick = function () {
+        omikuji.consult();
+    };
+};
+//# sourceMappingURL=index_ts.js.map
---
+++ omikuji_ts/index_ts.ts
@@ -0,0 +1,24 @@
+﻿class Omikuji {
+    span: HTMLElement;
+    items: Array<string>;
+
+    constructor(span: HTMLElement) {
+        this.span = span;
+        this.items = ["大吉", "中吉", "小吉", "凶", "大凶"];
+    }
+
+    consult() {
+        var result = Math.floor(Math.random() * this.items.length);
+        this.span.innerHTML = this.items[result];
+    }
+}
+
+window.onload = () => {
+    var result = document.getElementById("result");
+    var omikuji = new Omikuji(result);
+    var button = document.getElementById("omikuji");
+    button.onclick = () => {
+        omikuji.consult();
+    }
+}
+
